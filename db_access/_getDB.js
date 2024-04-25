6a14b3923c1ec94a1ac68392eb73744e1c6c13b4
michaelsupercode <96441437+michaelsupercode@users.noreply.github.com>
2024-04-25 11:33:36 +0200

    Update _getDB.js

--- db_access/_getDB.js
+++ db_access/_getDB.js
@@ -7,7 +7,7 @@ async function _getDB() {
   if (_db) {
     return _db;
   } else {
-    const url = process.env.API_URL;
+    const url = process.env.DB_URL;
     const client = new MongoClient(url);
 
     const connected_client = await client.connect();
